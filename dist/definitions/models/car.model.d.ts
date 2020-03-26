3ae858c4ada9e6ca23e10e1e503b400130b9420c
Akaljot Gujjar <akaljotgujjar@gmail.com>
2020-03-25 20:10:26 -0700

    finished get engpoints far car models

--- dist/definitions/models/car.model.d.ts
+++ dist/definitions/models/car.model.d.ts
@@ -1,5 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
 export declare class Car {
     _model: any;
     constructor(norm: any);
+    getAllCars(model: any): (req: Request<import("../../../../../../Users/akaljotgujjar/Desktop/ExamAPI/node_modules/@types/express-serve-static-core").ParamsDictionary>, res: Response<any>, next: NextFunction) => Promise<void>;
+    getCarByID(model: any): (req: Request<import("../../../../../../Users/akaljotgujjar/Desktop/ExamAPI/node_modules/@types/express-serve-static-core").ParamsDictionary>, res: Response<any>, next: NextFunction) => Promise<void>;
     model: any;
 }
