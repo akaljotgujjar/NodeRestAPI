96799436bed253c5747edd356622d90e15ceee6a
Akaljot Gujjar <akaljotgujjar@gmail.com>
2020-04-08 12:17:53 -0700

    made changes to code

--- dist/models/index.js
+++ dist/models/index.js
@@ -6,4 +6,3 @@ Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./test.model"));
 __export(require("./exam.model"));
 __export(require("./car.model"));
-__export(require("./book.model"));
